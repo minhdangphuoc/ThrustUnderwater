9f213f9090d6edb59f8242af66b0f146725e9351
DatPT02 <tiendat161002@gmail.com>
2021-10-12 12:04:43 +0300

    Orb collision, Power script to be updated

--- Assets/Scripts/MovementNew.cs
+++ Assets/Scripts/MovementNew.cs
@@ -70,7 +70,7 @@ public class MovementNew : MonoBehaviour
     /// <param name="other">The other Collider2D involved in this collision.</param>
     void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.GetComponent<Power>()!= null) Destroy(other.gameObject);
         Debug.Log("Collide");
-        Destroy(other.gameObject);
     }
 }
--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Orb : MonoBehaviour
+public class Power : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
